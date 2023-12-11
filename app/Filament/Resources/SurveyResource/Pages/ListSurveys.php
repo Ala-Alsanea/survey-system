6ae3675848e8f95efe627f62666e2c3bade08b79
Ala-Alsanea <ala0alsanea@gmail.com>
2023-12-11 16:13:44 +0300

    add filter tab to Survey table

--- app/Filament/Resources/SurveyResource/Pages/ListSurveys.php
+++ app/Filament/Resources/SurveyResource/Pages/ListSurveys.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\SurveyResource\Pages;
 
 use App\Filament\Resources\SurveyResource;
+use App\Models\Survey;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListSurveys extends ListRecords
@@ -14,6 +16,26 @@ class ListSurveys extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'reviewed' => Tab::make('reviewed')
+            ->badge(Survey::where('done',1)->count())
+            ->modifyQueryUsing(function ($query){
+               return $query->where('done',1);
+            }),
+            'not reviewed' => Tab::make('not reviewed')
+                ->badge(Survey::where('done', 0)->count())
+
+            ->modifyQueryUsing(function ($query) {
+                return $query->where('done', 0);
+            })
+                ,
+            'all' => Tab::make('all')
+            ->badge(Survey::count()),
         ];
     }
 }
