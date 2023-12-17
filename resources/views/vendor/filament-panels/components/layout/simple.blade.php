0aa6b30f5bd0c6e474ebf0eb57de944e1b9f898c
Ala-Alsanea <ala0alsanea@gmail.com>
2023-12-18 00:37:27 +0300

    login page bg color

--- resources/views/vendor/filament-panels/components/layout/simple.blade.php
+++ resources/views/vendor/filament-panels/components/layout/simple.blade.php
@@ -8,29 +8,25 @@
         'heading' => null,
         'subheading' => null,
     ])
+    <div style="background-color: #0198F1">
 
+        <img src="/img/logo_1.jpeg" width="400" style="position: fixed; z-index:10; bottom: 60px" alt="">
+        <img src="/img/logo_2.jpeg" width="400" style="position: fixed; z-index:10; bottom: 30px; right:1px ;"
+            alt="">
 
-<img src="/img/logo_1.jpeg" width="400" style="position: fixed; z-index:10; bottom: 60px" alt="">
-<img src="/img/logo_2.jpeg" width="400" style="position: fixed; z-index:10; bottom: 30px; right:1px ;" alt="">
+        <div class="fi-simple-layout flex min-h-screen flex-col items-center">
+            @if (filament()->auth()->check())
+                <div class="absolute end-0 top-0 flex h-16 items-center gap-x-4 pe-4 md:pe-6 lg:pe-8">
+                    @if (filament()->hasDatabaseNotifications())
+                        @livewire(Filament\Livewire\DatabaseNotifications::class, ['lazy' => true])
+                    @endif
 
-    <div class="fi-simple-layout flex min-h-screen flex-col items-center">
-        @if (filament()->auth()->check())
-            <div
-                class="absolute end-0 top-0 flex h-16 items-center gap-x-4 pe-4 md:pe-6 lg:pe-8"
-            >
-                @if (filament()->hasDatabaseNotifications())
-                    @livewire(Filament\Livewire\DatabaseNotifications::class, ['lazy' => true])
-                @endif
+                    <x-filament-panels::user-menu />
+                </div>
+            @endif
 
-                <x-filament-panels::user-menu />
-            </div>
-        @endif
-
-        <div
-            class="fi-simple-main-ctn flex w-full flex-grow items-center justify-center"
-        >
-            <main
-                @class([
+            <div class="fi-simple-main-ctn flex w-full flex-grow items-center justify-center">
+                <main @class([
                     'fi-simple-main my-16 w-full bg-white px-6 py-12 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 sm:rounded-xl sm:px-12',
                     match ($maxWidth ?? null) {
                         MaxWidth::ExtraSmall, 'xs' => 'sm:max-w-xs',
@@ -45,13 +41,14 @@
                         MaxWidth::SevenExtraLarge, '7xl' => 'sm:max-w-7xl',
                         default => 'sm:max-w-lg',
                     },
-                ])
-            >
-                {{ $slot }}
-            </main>
-        </div>
+                ])>
+                    {{ $slot }}
+                </main>
+            </div>
 
-        {{ \Filament\Support\Facades\FilamentView::renderHook('panels::footer') }}
+            {{ \Filament\Support\Facades\FilamentView::renderHook('panels::footer') }}
+        </div>
     </div>
+
 
 </x-filament-panels::layout.base>
