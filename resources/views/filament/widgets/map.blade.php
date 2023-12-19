e99991e43bba0c0fc3d2de04a3f2d0d7d0e49516
Ala-Alsanea <ala0alsanea@gmail.com>
2023-12-19 10:28:09 +0300

    modified:   resources/views/filament/widgets/map.blade.php

--- resources/views/filament/widgets/map.blade.php
+++ resources/views/filament/widgets/map.blade.php
@@ -1,11 +1,11 @@
 <x-filament-widgets::widget>
-    <x-filament::section>
+    <x-filament::section >
 
         {{-- <script src="https://code.jquery.com/jquery-3.4.1.js"></script> --}}
 
         {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
 
-        <iframe src="{{url('/map')}}" width="100%" height="400px"></iframe>
+        <iframe src="{{url('/map')}}" width="100%" height="500px"></iframe>
 
 
     </x-filament::section>
